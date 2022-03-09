8f85ee100a3ffe0490dfad447404722456fe33d4
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-03-09 12:22:56 +0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  opens: boolean = false;
+  constructor(
+    public routerOutlet: IonRouterOutlet
+  ) {}
 
 }
